1ef993a810055ea2b1b90161e3c4c3354c768ffd
Owen Dyer <69588466+owensdyer@users.noreply.github.com>
2025-12-18 23:49:35 +0000

    Add AddDeviceWindow

---
+++ SSHClient/AddDeviceWindow.xaml.cs
@@ -0,0 +1,27 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SSHClient
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddDeviceWindow : Window
+    {
+        public AddDeviceWindow()
+        {
+            InitializeComponent();
+
+            // Configure window
+            var handle = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(handle);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new Windows.Graphics.SizeInt32(400, 400));
+            appWindow.Title = "SSH Client - Add Device";
+        }
+    }
+}
