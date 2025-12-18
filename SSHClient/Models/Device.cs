461f63f133a07355da421a5b8d47f7a6030d9fa4
Owen Dyer <69588466+owensdyer@users.noreply.github.com>
2025-12-18 14:18:15 +0000

    Put device in its own file

---
+++ SSHClient/Models/Device.cs
@@ -0,0 +1,19 @@
+﻿// - - - - -
+// Device.cs
+// Models the SSH device with necessary connection details.
+// - - - - -
+
+namespace SSHClient.Models
+{
+    public class Device
+    {
+        public required string Nickname { get; set; }
+        public required string IPAddress { get; set; }
+        public required string Username { get; set; }
+        public string Password { get; set; }
+        public string CertificatePath { get; set; }
+
+        // The key used in Credential Manager
+        public string CredentialKey => $"{Username}@{IPAddress}";
+    }
+}
