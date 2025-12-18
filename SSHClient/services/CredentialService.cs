335676973330e79d0fee812dfab578cf1aa2ef7a
Owen Dyer <69588466+owensdyer@users.noreply.github.com>
2025-12-18 13:33:52 +0000

    Add Windows Credential Manager service

---
+++ SSHClient/services/CredentialService.cs
@@ -0,0 +1,32 @@
+﻿using CredentialManagement;
+
+namespace SSHClient.services
+{
+    public static class CredentialService
+    {
+        // Save password for a device
+        public static void SavePassword(string key, string password)
+        {
+            var cred = new Credential
+            {
+                Target = key,
+                Password = password,
+                PersistanceType = PersistanceType.LocalComputer,
+                Type = CredentialType.Generic
+            };
+            cred.Save();
+        }
+
+        public static string GetPassword(string key)
+        {
+            var cred = new Credential { Target = key };
+            return cred.Load() ? cred.Password : null;
+        }
+
+        public static void DeletePassword(string key)
+        {
+            var cred = new Credential { Target = key };
+            cred.Delete();
+        }
+    }
+}
