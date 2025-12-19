103e857c43d5152d779d85a09c8ebe2d0f2ccbd7
Owen Dyer <69588466+owensdyer@users.noreply.github.com>
2025-12-19 02:03:29 +0000

    Refactor for adding DeviceAccounts

--- SSHClient/Models/Device.cs
+++ SSHClient/Models/Device.cs
@@ -3,14 +3,15 @@
 // Models the SSH device with necessary connection details.
 // - - - - -
 
+using System.Collections.Generic;
+
 namespace SSHClient.Models
 {
     public class Device
     {
         public required string Nickname { get; set; }
         public required string IPAddress { get; set; }
-        public required string Username { get; set; }
-        public string? Password { get; set; }
         public string? CertificatePath { get; set; }
+        public List<DeviceAccount> Accounts { get; set; } = new();
     }
 }
---
+++ SSHClient/Models/DeviceAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSHClient.Models
+{
+    /// <summary>
+    /// Represents the credentials and certificate information for a device account.
+    /// </summary>
+    public class DeviceAccount
+    {
+        public required string username { get; set; }
+        public string? password { get; set; }
+        public string? CertificatePath { get; set; }
+    }
+}
